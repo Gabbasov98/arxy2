794ac539586295493b7ee8cd4274a0b97de3b7b4
Marsel Gabbasov <marsel13_98@mail.ru>
2022-04-28 00:40:07 +0500

    ыввы

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -31,6 +31,9 @@ function trustSliders() {
 
 
 $(document).ready(function() {
+    $('input[type="tel"]').mask('+7 999 999-9999', { placeholder: '+7             ' });
+
+
     if (window.innerWidth < 768) {
         portfolioSlider()
     }
--- tasks/libs_style.js
+++ tasks/libs_style.js
@@ -1,6 +1,7 @@
 const plugins = [
     'node_modules/bootstrap/dist/css/bootstrap.css',
     'node_modules/swiper/swiper-bundle.css',
+    'src/libs/css/jquery.fancybox.min.css',
 ];
 
 const {
