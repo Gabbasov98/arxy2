ccd403efa6e51d5c342448db580da632915621f2
Marsel Gabbasov <marsel13_98@mail.ru>
2022-04-29 22:01:36 +0500

    рпрп

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,9 +1,24 @@
 function portfolioSlider() {
     var swiper = new Swiper('.portfolio .swiper-container', {
-        slidesPerView: "auto",
+        slidesPerView: 2.2,
         spaceBetween: 10,
+        breakpoints: {
+            320: {
+                slidesPerView: 1.3,
+            },
+            450: {
+                slidesPerView: 1.8,
+            },
+            500: {
+                slidesPerView: 2,
+            },
+            600: {
+                slidesPerView: 2.2,
+            },
+        }
     })
 }
+
 
 let portfolioSlidersArray = []
 
@@ -48,13 +63,13 @@ $(document).ready(function() {
     })
 
 
-    $('.video').parent().click(function() {
-        if ($(this).children(".video").get(0).paused) {
-            $(this).children(".video").get(0).play();
-            $(this).children(".playpause").fadeOut();
-        } else {
-            $(this).children(".video").get(0).pause();
-            $(this).children(".playpause").fadeIn();
-        }
-    });
+    // $('.video').parent().click(function() {
+    //     if ($(this).children(".video").get(0).paused) {
+    //         $(this).children(".video").get(0).play();
+    //         $(this).children(".playpause").fadeOut();
+    //     } else {
+    //         $(this).children(".video").get(0).pause();
+    //         $(this).children(".playpause").fadeIn();
+    //     }
+    // });
 });
